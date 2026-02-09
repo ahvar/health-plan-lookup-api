d6ed25d8841b6284fcdb2473f88b3d67b25ed870
Arthur Vargas <arthurvargasdev@gmail.com>
2026-02-08 18:47:38 -0600

    Set up shared SQLAlchemy engine, metadata, and session

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,22 @@
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+from app.config import Config
+from app.db import Model
+
+db = SQLAlchemy(model_class=Model, metadata=Model.metadata)
 migrate = Migrate()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
+
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
 
     from app.api import bp as api_bp
+
     app.register_blueprint(api_bp)
-
-    
-
-
-from app.api import plans
+    return app
